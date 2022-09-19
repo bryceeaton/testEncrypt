3482eac4fe2b6d4c311ca7777a17efe9e921ab65
bryceeaton <37537512+bryceeaton@users.noreply.github.com>
2022-09-19 16:08:04 +1000

    Update App.js

--- App.js
+++ App.js
@@ -7,6 +7,34 @@ function App() {
 const upload = async(imgURL, mp3URL) => {
 
 async function storeNFT() {
+  
+  const [encryptMp3, setEncryptMp3] = useState('');
+  const [encryptBase64, setEncryptBase64] = useState('');
+  const [song, setSong] = useState([]);
+  const [artists, setArtists] = useState([]);
+  const artistsRef = collection(db, "artists");
+
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+
+    const artistData = await getDocs(artistsRef);
+    setArtists(artistData.docs.map((doc) => ({...doc.data(), id: doc.id})));
+
+  }
+  
+  fetchData();
+}, []);
+
+
+const onClick = async (id) => {
+
+  const songsRef = collection(db, "artists", id, "songs");
+  const songData = await getDocs(songsRef);
+  setSong(songData.docs.map((doc) => ({...doc.data(), id: doc.id})));
+  
+};
 
   const url = await fetch("file url")
   const imgData = await url.blob()
@@ -30,5 +58,43 @@ async function storeNFT() {
 
 storeNFT()
 };
-  
+  return (
+    <div className="App">
+      <header className="header">
+        <img src={logo} className="logo" alt="logo" />
+        <h2>Admin Portal</h2>
+      </header>
+      <div className='content'>
+        <div className='table'>
+          <h2>Artists</h2>
+          {artists.map((artist, id) => {
+            return <div key={id} className='artistTable'>
+              <p 
+              onClick={() => {
+              onClick(artist.id)
+            }}>
+                {artist.id}
+              </p>
+              </div>
+            })}
+        </div>
+        <div className='songsContent'>
+          {song.map((song, id) => {
+            return <div key={id} className='songTable'>
+              <img className="coverImage" src={song.imgURL} />
+              <div className='songInfo'>
+              <p>{song.song}</p>
+              <a href={song.mp3URL}></a>
+                <button type="button" onClick={() => {
+              upload(song.imgURL, song.mp3URL)}} >
+                  Upload to IPFS
+                </button>
+              </div>
+            </div>
+            })}
+        </div>
+      </div>
+    </div>
+  );
+} 
 export default App;
