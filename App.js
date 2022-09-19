f25924b30507491cfe53b83b6b45ab0491b5ebef
bryceeaton <37537512+bryceeaton@users.noreply.github.com>
2022-09-19 15:55:58 +1000

    App.js

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import lit from './lit.ts';
+
+function App() {
+
+const upload = async(imgURL, mp3URL) => {
+
+async function storeNFT() {
+
+  const url = await fetch("file url")
+  const imgData = await url.blob()
+  const image = new File(
+    [imgData],
+    'image name',
+    {type: 'image/png'}
+  );
+
+  const songUrl = await fetch("file url")
+  const songData = await songUrl.blob()
+  const song = new File(
+    [songData],
+    'mp3 name',
+    {type: 'audio/mpeg'}
+  );
+
+
+  const { encryptedFile, encryptedSymmetricKey } = await lit.encrypt(song);
+  console.log(encryptedFile);
+
+storeNFT()
+
+};
